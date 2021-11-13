64e9660ef996a92887225117892202072aee5057
Lyndsey Pohl <lapohl@umich.edu>
2021-11-13 02:31:06 -0500

    starting Unity project

---
+++ Documents/Unity/Get the Gopher/Assets/Scripts/ClubMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClubMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        screenPoint.z = 10.0f; //distance of the plane from the camera
+        transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+
+    }
+}
